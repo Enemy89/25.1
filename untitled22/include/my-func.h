6aab1f0bf4ccb9c65e3276ebc63c9dc91539b927
Enemy89 <66246104+Enemy89@users.noreply.github.com>
2023-11-11 10:59:19 +0300

    Add files via upload

--- untitled22/include/my-func.h
+++ untitled22/include/my-func.h
@@ -1,41 +1,37 @@
 #pragma once
 
 struct coordinate {
-    double x1_scp;
-    double y1_scp;
-    double x2_scp;
-    double y2_scp;
-
-    double x1_str;
-    double y1_str;
-    double x2_str;
-    double y2_str;
-
-    double x1_one;
-    double y1_one;
+    double x;
+    double y;
 };
 
-coordinate scalpel;
-coordinate suture;
+coordinate scalpel_start;
+coordinate scalpel_finish;
+coordinate suture_start;
+coordinate suture_finish;
 
 void enter_coord_scp () {
-    std::cout<<"Enter the coordinates of the beginning and end of the section."<<std::endl;
     double user_x1, user_y1, user_x2, user_y2;
-    std::cin>>user_x1>>user_y1>>user_x2>>user_y2;
-    scalpel.x1_scp=user_x1;
-    scalpel.y1_scp=user_y1;
-    scalpel.x2_scp=user_x2;
-    scalpel.y2_scp=user_y2;
+    std::cout<<"Enter the coordinate of the beginning of the cut:"<<std::endl;
+    std::cin>>user_x1>>user_y1;
+    scalpel_start.x=user_x1;
+    scalpel_start.y=user_y1;
+    std::cout<<"Enter the coordinate of the end of the section:"<<std::endl;
+    std::cin>>user_x2>>user_y2;
+    scalpel_finish.x=user_x2;
+    scalpel_finish.y=user_y2;
 }
 
 void enter_coord_str () {
-    std::cout<<"Enter the coordinates of the beginning and end of the section."<<std::endl;
     double user_x1, user_y1, user_x2, user_y2;
-    std::cin>>user_x1>>user_y1>>user_x2>>user_y2;
-    suture.x1_str=user_x1;
-    suture.y1_str=user_y1;
-    suture.x2_str=user_x2;
-    suture.y2_str=user_y2;
+    std::cout<<"Enter the coordinate of the beginning of the seam:"<<std::endl;
+    std::cin>>user_x1>>user_y1;
+    suture_start.x=user_x1;
+    suture_start.y=user_y1;
+    std::cout<<"Enter the coordinate of the end of the seam:"<<std::endl;
+    std::cin>>user_x2>>user_y2;
+    suture_finish.x=user_x2;
+    suture_finish.y=user_y2;
 }
 
 void enter_coord() {
@@ -43,13 +39,13 @@ void enter_coord() {
     double user_x1, user_y1;
     std::cin>>user_x1>>user_y1;
     coordinate one;
-    one.x1_one=user_x1;
-    one.y1_one=user_y1;
+    one.x=user_x1;
+    one.y=user_y1;
 }
 
 void check(int* count) {
-    if ((scalpel.x1_scp == suture.x1_str) && (scalpel.y1_scp == suture.y1_str) && (scalpel.x2_scp == suture.x2_str) &&
-        (scalpel.y2_scp == suture.y2_str)) {
+    if ((scalpel_start.x == suture_start.x) && (scalpel_start.y == suture_start.y) && (scalpel_finish.x == suture_finish.x) &&
+        (scalpel_finish.y == suture_finish.y)) {
         *count = 1;
         std::cout << "The seam is completed." << std::endl;
     } else
